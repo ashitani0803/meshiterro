80e099e0eb9c8509d14793db6dc517a6c94638a9
ashitani0803 <ashitani0803@gmail.com>
2019-04-30 06:25:17 +0000

    [Finish] 1-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8cc66b0b5efd4147d8c6fe0fd187faa77087fbdb250c639a72865a4e9a88b5136c7a8904cc6dad3b4067974ab237a29a81178ad6d607aaa8923ebd01c2ece95b'
